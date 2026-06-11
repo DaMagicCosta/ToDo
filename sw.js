52d66f9d2e84a5b333ab199517b309c2df83ff7b
Alexander da Costa Amaral <da.costa.amaral86@gmail.com>
2026-06-11 02:49:47 +0200

    update todos

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'todo-shell-v1';
+const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('todos.enc.json')) return;  // Daten immer vom Netz
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
