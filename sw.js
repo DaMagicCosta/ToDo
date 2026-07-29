ed8c0a87bde31f44bac1d4fae39545ae16b286ae
Alexander da Costa Amaral <da.costa.amaral86@gmail.com>
2026-07-29 19:49:06 +0200

    update todos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'todo-shell-515a4a9b9745';
+const CACHE = 'todo-shell-7a7f18c86c33';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
